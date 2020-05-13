0a2c930f69c4f28a919b01b47774f5d5dd72362c
SoyDiego <diegofranchina@gmail.com>
2020-05-13 04:44:36 +0200

    Moved /img to /public

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from "react";
 import "./css/styles.css";
 import "./css/responsive.css";
 import Form from "./Components/Form";
-import './img/background.jpg'
 
 function App() {
 	return (
