4ad22d536a87751fd4881c026d8a406a4b046296
SoyDiego <diegofranchina@gmail.com>
2020-05-13 04:22:52 +0200

    Added import background

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import "./css/styles.css";
 import "./css/responsive.css";
 import Form from "./Components/Form";
+import './img/background.jpg'
 
 function App() {
 	return (
